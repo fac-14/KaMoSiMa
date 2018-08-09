d51e15fd6c8bdc51e298bd2395aee1968284ee41
martingaston <squidman@gmail.com>
2018-08-09 13:03:25 +0100

    Add more database queries and tests

---
+++ tests/test.dbQueries.js
@@ -0,0 +1,38 @@
+const tape = require('tape');
+const dbQuery = require('../src/dbQueries.js');
+
+const dbBuild = require('../db/db_build');
+
+tape('check tape is working', (t) => {
+    t.equal(2 + 2, 4, '2+2=4');
+    t.end();
+});
+
+tape("getData", t => {
+    dbQuery.getDoggo((err, res) => {
+        if (err) { console.log("Balls!"); }
+        let actual = res.rows;
+        let expected = [{
+            dog_name: 'Fenton',
+            dog_breed: 'Labrador',
+            park_name: 'Richmond Park'
+        },
+        {
+            dog_name: 'Mackerel',
+            dog_breed: 'Beagle',
+            park_name: 'Victoria Park'
+        },
+        {
+            dog_name: 'Mackerel',
+            dog_breed: 'Beagle',
+            park_name: 'Finsbury Park'
+        },
+        {
+            dog_name: 'Spot',
+            dog_breed: 'Beagle',
+            park_name: 'Hampstead Heath'
+        }];
+        t.deepEqual(actual, expected, "Returned Four Great Dogs!");
+        t.end();
+    })
+});
