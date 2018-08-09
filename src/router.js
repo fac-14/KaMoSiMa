a6583c4c4962508a9d23c9b79b7da376f7261dd7
Simon Dupree <dupreenator@gmail.com>
2018-08-09 11:47:33 +0100

    supertesting for endpoints home and public > relates #5

--- src/router.js
+++ src/router.js
@@ -2,23 +2,44 @@ const http = require('http');
 const fs = require('fs');
 const pg = require('pg');
 const querystring = require('querystring');
+const mime = require('mime-types');
+const path = require('path');
+
+const buildPath = (mypath) => {
+    return path.join(__dirname, '..', mypath);
+}
 
 const router = (request, response) => {
     const endpoint = request.url.split('/')[1];
+    const extension = request.url.split('/')[2];
+    console.log('request.url', request.url);
+    console.log('endpoint', endpoint);
+    console.log('extension', extension);
 
     if (endpoint === '') {
         fs.readFile(__dirname + '/../public/index.html', (err, file) => {
             if (err) {
-                response.writeHead(500, { 'content-type': 'text/html' });
+                response.writeHead(500, { 'content-type': 'text/plain' });
                 response.end('file not found, sry');
             } else {
                 response.writeHead(200, { 'content-type': 'text/html' });
                 response.end(file);
             }
         });
+    } else if (request.url.includes('public')) {
+        fs.readFile(buildPath(request.url), (err, file) => {
+            console.log("buildPath ", buildPath(request.url));
+            if (err) {
+                response.writeHead(500, { 'content-type': 'text/plain' });
+                response.end('server error');
+            }
+            response.writeHead(200, { 'content-type': mime.lookup(request.url) });
+            response.end(file);
+        });
+
     } else {
-        response.writeHead(500, { 'content-type': 'text/html' });
-        response.end('No page');
+        response.writeHead(404, { 'content-type': 'text/html' });
+        response.end('Page doesn´t exist');
     }
 }
 
--- tests/test.server.js
+++ tests/test.server.js
@@ -1,6 +1,45 @@
-const tape = require('tape');
+const test = require('tape');
+const router = require('../src/router');
+const supertest = require('supertest');
 
-tape('check tape is working', (t) => {
+test('check tape is working', (t) => {
   t.equal(1, 1, 'One equals one except when Big Brother is watching');
   t.end();
+});
+
+test('home route', (t) => {
+  supertest(router)
+    .get('')
+    .expect(200)
+    .expect('content-type', /html/)
+    .end((err, res) => {
+      t.error(err)
+      t.equal(res.status, 200, 'response status should be 200')
+      t.end();
+    });
+});
+
+test('check status code 404', (t) => {
+  supertest(router)
+    .get('/myerrordog')
+    .expect(404)
+    .expect('content-type', /html/)
+    .end((err, res) => {
+      t.error(err)
+      t.equal(res.status, 404, 'response status should be 404')
+      t.equal(res.text, 'Page doesn´t exist', 'response should contain Page doesn´t exist')
+      t.end();
+    });
+});
+
+test('check if public assets are loading', (t) => {
+  supertest(router)
+    .get('/public/main.css')
+    .expect(200)
+    .expect('content-type', /css/)
+    .end((err, res) => {
+      t.error(err)
+      t.equal(res.status, 200, 'response status should be 200')
+      t.end();
+    });
 });
