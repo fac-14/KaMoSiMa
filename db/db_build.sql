aa406e99ba8309c1da750f02b9243f1efe11e27b
martingaston <squidman@gmail.com>
2018-08-15 15:57:14 +0100

    Update db_build for week 7 challenge

--- db/db_build.sql
+++ db/db_build.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-DROP TABLE IF EXISTS dogs, spots, parks CASCADE;
+DROP TABLE IF EXISTS dogs, spots, parks, users CASCADE;
 
 CREATE TABLE dogs (
     id SERIAL PRIMARY KEY,
@@ -25,12 +25,23 @@ INSERT INTO parks (park_name, lat, lon) VALUES
 ('Richmond Park', 51.4469992, -0.2867548),
 ('Victoria Park', 51.5365614, -0.0411607);
 
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    is_admin BOOLEAN NOT NULL,
+    pass VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+INSERT INTO users (username, is_admin, pass, email) VALUES ('sbinlondon', true, 'Doggo12', 'kate@email.com'), ('dupreesi', false, 'wunderbar', 'simon@email.com');  
+
 CREATE TABLE spots (
     id SERIAL PRIMARY KEY,
     dog_id INTEGER REFERENCES dogs(id),
-    park_id INTEGER REFERENCES parks(id)
+    park_id INTEGER REFERENCES parks(id),
+    user_id INTEGER REFERENCES users(id)
 );
 
-INSERT INTO spots (dog_id, park_id) VALUES (2, 5), (1, 6), (1, 2), (3, 4);
+INSERT INTO spots (dog_id, park_id, user_id) VALUES (2, 5, 1), (1, 6, 1), (1, 2, 2), (3, 4, 2);
 
 COMMIT;
